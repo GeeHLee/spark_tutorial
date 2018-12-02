a0b9d052bcdeb534fcae7f41e0be09b0650413c4
liheng <liheng87@gmail.com>
2018-12-02 17:34:37 +0100

    end of KV RDD tutorial

--- main.py
+++ main.py
@@ -48,7 +48,8 @@ action_reduce = rdd.reduce(lambda a, b: a if a > b else b)
 print(action_reduce)
 
 # cache:
-# since rdd will not execute (charge in the cache) untill a given action, so if we need more then one action, spark will start over on each time.
+# since rdd will not execute (charge in the cache) until a given action.
+# so if we need more then one action, spark will start over on each time.
 # Therefore the solution is charge the RDD in the cache manually.
 
 list = ["Hadoop", "Spark", "Hive"]
@@ -67,3 +68,41 @@ list = [1,2,3,4,5]
 rdd = sc.parallelize(list, 2)
 
 #%% key value RDD:
+# create pair RDD from local file.
+lines = sc.textFile("data/words.txt")
+pair_rdd = lines.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1))
+pair_rdd.foreach(print)
+# create pair RDD from list.
+list = ["Hadoop", "Spark", "Hive", "Spark"]
+rdd = sc.parallelize(list)
+rdd.cache() # since following example will use this rdd.
+pair_rdd = rdd.map(lambda word: (word, 1))
+pair_rdd.foreach(print)
+
+# common transformation operation for pair RDD:
+# reduceByKey: reduce value by keys using costume functions.
+pair_rdd.reduceByKey(lambda a, b: a + b).foreach(print)
+# groupByKey: group by key in KV RDD.
+pair_rdd.groupByKey().foreach(print)
+# keys: create a new RDD which contain only the keys.
+pair_rdd.keys().foreach(print)
+# values: create a new RDD which contain only the values.
+pair_rdd.values().foreach(print)
+# sortByKey: sort a KV RDD by keys.
+pair_rdd.sortByKey().foreach(print)
+# mapValues: operation only affect the values.
+pair_rdd.mapValues(lambda x: x * 2).foreach(print)
+# join: include join, leftOuterJoin, rightOuterJoin ...
+pair_rdd1 = sc.parallelize([('spark', 1), ('spark', 2), ('hadoop', 3), ('hadoop', 5)])
+pair_rdd2 = sc.parallelize([('spark', 'fast')])
+pair_rdd1.leftOuterJoin(pair_rdd2).foreach(print)
+pair_rdd1.join(pair_rdd2).foreach(print)
+
+# example: calculate the average volume of a KV RDD given.
+rdd = sc.parallelize([("spark", 2), ("hadoop", 6), ("hadoop", 4), ("spark", 6)])
+# mapValues: create a count value in tuple.
+# reduceByKey: 0 index for volume, 1 index for count
+# second mapValues: calculation of average.
+rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1])).mapValues(lambda x: (x[0]/x[1])).collect()
+
+#%% broadcast variables:
